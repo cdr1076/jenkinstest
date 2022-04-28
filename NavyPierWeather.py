c78571e5db6a90391e428a99b9742419b8bb54b8
cdr1076 <86681100+cdr1076@users.noreply.github.com>
2022-04-28 15:19:09 -0500

    Create NavyPierWeather.py

---
+++ NavyPierWeather.py
@@ -0,0 +1,22 @@
+
+
+NAVYPIERWEATHER = "https://api.weather.gov/gridpoints/LOT/75,73/forecast"
+
+# python3 -m pip install requests
+import requests
+
+def main():
+
+    # make a request via requests library
+    chiweather = requests.get(NAVYPIERWEATHER)
+    
+    # remove the json attached to the 200 response
+    chiweather = chiweather()
+
+    # display just the list (array) of space travelers
+    print(chiweather.get("name"))
+    print(chiweather.get("icon"))
+    print(chiweather.get("detailedForecast"))
+    
+if __name__ == "__main__":
+    main()
